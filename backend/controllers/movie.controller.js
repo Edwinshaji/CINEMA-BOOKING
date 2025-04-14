89e852e23976a4d4eb9fba15983038820b689dc0
Edwin Shaji <edwinshajioffical@gmail.com>
2025-04-14 23:24:23 +0530

    created api's for addMovie,editMovie,deleteMovie,getAllMovies

---
+++ backend/controllers/movie.controller.js
@@ -0,0 +1,74 @@
+import Movie from "../models/movie.model.js";
+import fs from 'fs';
+import path from 'path'
+
+export const addMovie = async (req, res) => {
+    let { title, description, duration, language } = req.body;
+
+    try {
+        // checking if all the fields are submitted by the user 
+        if (!title || !description || !duration || !language) {
+            return res.status(400).json({ status: false, message: "All fields required" })
+        }
+
+        // creating a new movie document 
+        const newMovie = new Movie({ title, description, duration, language, posterUrl: req.file.filename, isActive: true })
+        await newMovie.save()
+
+        return res.status(201).json({ status: true, message: "Movie added successfuly", newMovie });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: false, message: "Server Error" })
+    }
+}
+
+export const editMovie = async (req, res) => {
+    let movieId = req.params.id; // accessing the movie id
+
+    try {
+        // finding and updating the movie  
+        await Movie.findByIdAndUpdate(movieId, req.body)
+        return res.status(200).json({ status: true, message: "Movie updated successfuly" })
+    } catch (error) {
+        return res.status(500).json({ status: false, message: "Server Error" })
+    }
+}
+
+export const deleteMovie = async (req, res) => {
+    let movieId = req.params.id;
+
+    try {
+        // acquires the movie data from the database with the moviId
+        let movie = await Movie.findById(movieId);
+        if (!movie) {
+            return res.status(404).json({ message: "Movie not found" })
+        }
+        // making the path of the image with the posterUrl
+        const posterPath = path.join('C:/Users/Lenovo/Desktop/PROJECTS/CINEMA BOOKING/backend/public/poster-images' , movie.posterUrl);
+        // using fs unlink the item of the given path
+        fs.unlink(posterPath, (err) => {
+            if (err) {
+                console.log("Failed to delete the image form the server",err)
+            } else {
+                console.log("Image deleted ", posterPath);
+            }
+        })
+
+        // deleting the movie form the database
+        await Movie.findByIdAndDelete(movieId);
+        return res.status(200).json({ status: true, message: "Movie deleted successfuly" })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: false, message: "Server Error" })
+    }
+}
+
+export const getAllMovies = async (req, res) => {
+    try {
+        let movies = await Movie.find();
+        return res.status(200).json(movies)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: false, message: "Server Error" });
+    }
+}
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import multer from "multer";
+import path from 'path'
+import { addMovie, deleteMovie, editMovie, getAllMovies } from '../controllers/movie.controller.js';
 
 const router = express.Router();
 
-router.get("/",()=>{
-
+// multer package is for storing the file that we get from the frontend to the server
+// defining the storage that is where to store the image
+const storage = multer.diskStorage({
+    destination:function (req,res,cb){
+        cb(null,'backend/public/poster-images');
+    },
+    filename:function (req,file,cb){
+        cb(null,file.fieldname+'_'+Date.now()+path.extname(file.originalname))
+    }
 })
+// creating an object of the multer with the storage definition
+const upload=multer({storage})
+
+router.post("/addMovie",upload.single('poster_image'),addMovie); // api for adding a new movie
+
+router.put('/editMovie/:id',editMovie); // api for editing an existing movie
+
+router.delete('/deleteMovie/:id',deleteMovie); // api for deleting a movie
+
+router.get('/getAllMovies',getAllMovies); // api for getting all movies from the databse
 
 export default router;
