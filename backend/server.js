820fc11a6182bcdcfb1ace4c7f222c2995e92bfb
Edwin Shaji <edwinshajioffical@gmail.com>
2025-06-26 12:06:58 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config()
 
 app.use(cors({
+    origin: 'http://localhost:5173/',
     origin: 'https://cinego-m8y6.onrender.com',
     credentials: true
 }));
